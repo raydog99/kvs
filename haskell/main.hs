36a24dbddf7417d9a8518cf242c6b44300267fb4
Raymond Deng <1107450@gmail.com>
2023-08-24 03:57:12 -0700

    update some stubs

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,4 +1,22 @@
-import System.Environment
+module Chord(
+	NodeId,
+	NodeState(...),
+	FingerTable,
+	bootstrap,
+	findSuccesors,
+	successors,
+	successor,
+	getState
+	) where
 
-main :: IO ()
-main = getArgs >>= print . haqify . head
+import Data.Binary
+import Data.Int (Int64)
+import GHC.Generics
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.MVar
+import Control.Monad
+
+import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node
+
--- ocaml/main.ml
+++ ocaml/main.ml
@@ -1,5 +1,35 @@
-open Base
-open Stdio
+module B = Bencode
+module R = Rpc
 
-let () =
-	printf "todotodtodotdotod\n"
+module type RPC = Rpc.S
+
+module type CONFIG = sig
+	val dimension: int
+	val redundancy: int
+	val finger_frequency: int
+	val node_timeout: int
+	val timeout: int
+end
+
+module ConfigDefault : CONFIG = struct
+	let dimension = 100
+	let redundancy = 5
+	let finger_frequency = 5
+	let node_timeout = 300
+	let timeout = 3
+end
+
+module type S = sig
+	module Config : CONFIG
+	module Rpc : RPC.S
+
+	(* node UID *)
+	module ID : sig
+		type t
+
+		val of_string : string -> t
+		val to_string : t -> string
+		val eq : t -> t -> bool
+		val hash : t -> int
+	end
+end
