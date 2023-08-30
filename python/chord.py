640796a15829a8fc880ddf3723fd2c60e5c83cd5
Raymond Deng <1107450@gmail.com>
2023-08-30 13:18:25 -0700

    add node init

--- python/chord.py
+++ python/chord.py
@@ -4,10 +4,30 @@ import threading
 import random
 import time
 import mutex
+import hashlib
 
 class Node(object):
-	def __init__(self):
-		pass
+	def __init__(self, ip_addr, finger = [], successor = None, predecessor = None):
+		self.ip_addr = ip_addr
+		self.finger = finger
+		self.successor = successor
+		self.predecessor = predecessor
+
+	def find_successor(self, id):
+		node = find_successor(id)
+		return node.successor
+
+	def find_predecessor(self, id):
+		n_prime = self
+		while (id != [n_prime, n_prime.successor]):
+			n_prime = n_prime.closest_preceding_finger(id)
+		return n_prime
+
+	def closest_preceding_finger(self, id):
+		for i in range(m, 0, -1):
+			if finger[i].node in [n, id]:
+				return finger[i].node
+		return n
 
 if __name__ == "__main__":
 	import sys
