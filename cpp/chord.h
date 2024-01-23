6be46486f773179aff3a81b7776332c267e3921a
Raymond Deng <1107450@gmail.com>
2024-01-22 17:17:07 -0800

    add chord base protocol header

--- cpp/chord.h
+++ cpp/chord.h
@@ -1,10 +1,18 @@
 #ifndef _CHORD_H
 #define _CHORD_H
 
-using namespace std;
+class ChordNode{
+	ChordNode find_successor(id);
+	ChordNode find_predecessor(id);
+	ChordNode closest_preceding_finger(id);
 
-class Chord{
-
+	ChordNode successor;
+	ChordNode predecessor;
+private:
+	// 48 bits for IPv4 (32 bits) + port (16 bits)
+	// 144 bits for IPv6 (128 bits) + port (16 bits)
+	string ipaddr;
+	vector<ChordNode>finger_table;
 }
 
-#endifs
+#endif
