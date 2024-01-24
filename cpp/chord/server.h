3bea91531dc5576bde7efb7f1fa70ffe33d7ee2b
Raymond Deng <1107450@gmail.com>
2024-01-23 18:18:17 -0800

    update server header

--- cpp/chord/server.h
+++ cpp/chord/server.h
@@ -1,11 +1,22 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
+#include "chord.h"
+#include <string>
 #include <stdio.h>
 
-class Server{
+class Server : ChordNode{
+public:
+	Server(const std::string& ip);
+    virtual ~Server();
+
+    ChordNode* find_successor(std::string id) override;
+    ChordNode* find_predecessor(std::string id) override;
+    ChordNode* closest_preceding_finger(std::string id) override;
 private:
-	string ipaddr;
-}
+	// 48 bits for IPv4 (32 bits) + port (16 bits)
+	// 144 bits for IPv6 (128 bits) + port (16 bits)
+	std::string ipAddr;
+};
 
 #endif
