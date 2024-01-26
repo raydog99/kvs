b1e58930a95f222f25ff9e3b70ee7a3c391f4c72
Raymond Deng <1107450@gmail.com>
2024-01-25 17:20:29 -0800

    scrap

--- cpp/chord/server.h
+++ cpp/chord/server.h
@@ -1,9 +1,27 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
+#include "chord.h"
 #include <string>
 #include <stdio.h>
 
-void printMessage(int ip);
+class Server : ChordNode{
+public:
+	Server(const std::string& ip);
+    virtual ~Server() override;
+
+    ChordNode* find_successor(std::string id) override;
+    ChordNode* find_predecessor(std::string id) override;
+    ChordNode* closest_preceding_finger(std::string id) override;
+private:
+	std::string ipAddr;
+};
+
+
+
+
+
+
+
 
 #endif
