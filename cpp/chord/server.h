cec278c281d9dff25909a59b34c8b9d591761ee7
Raymond Deng <1107450@gmail.com>
2024-02-02 20:35:37 -0800

    update server header

--- cpp/chord/server.h
+++ cpp/chord/server.h
@@ -1,27 +1,48 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-#include <string>
 #include "ChordProtocol.capnp.h"
+#include "helper_functions.h"
+
+#include <string>
 #include <stdio.h>
+#include <iostream>
+
+#include <capnp/rpc.h>
+#include <capnp/message.h>
+#include <capnp/rpc-twoparty.h>
+
+#include <kj/debug.h>
+#include <kj/thread.h>
+#include <kj/compat/gtest.h>
+#include <kj/async-io.h>
 
 class ChordNodeImpl final: public ChordNode::Server {
 public:
-    explicit ChordNodeImpl(const std::string& nodeId);
+    ChordNodeImpl(std::string ipAddress);
+
     kj::Promise<void> findSuccessor(FindSuccessorContext context) override;
     kj::Promise<void> findPredecessor(FindPredecessorContext context) override;
     kj::Promise<void> closestPrecedingFinger(ClosestPrecedingFingerContext context) override;
-    kj::Promise<void> updateFingerTable(UpdateFingerTableContext context) override;
-    void join(NodeInfo node) override;
-    void initFingerTable(InitFingerTableContext context) override;
 
-    NodeInfo getSuccessor();
-    NodeInfo getPredecessor();
+    kj::Promise<void> join(JoinContext context) override;
+    kj::Promise<void> notify(NotifyContext context) override;
+
+    void stabilize();
+    void fixFingers();
+
+    kj::Promise<void> getSuccessor(GetSuccessorContext context) override;
+    kj::Promise<void> getPredecessor(GetPredecessorContext context) override;
+    kj::Promise<void> inRange(InRangeContext context) override;
+    kj::Promise<void> getIdentifier(GetIdentifierContext context) override;
 private:
-	NodeInfo nodeInfo;
-	std::string ipAddress;
-	std::string identifier;
-	vector<NodeInfo::Reader> fingerTable;
-}
+    std::string ipAddress;
+    std::string identifier;
+    int fingerTableSize;
+	
+	std::vector<ChordNodeImpl*> fingerTable;
+	ChordNodeImpl* successor;
+	ChordNodeImpl* predecessor;
+};
 
 #endif
